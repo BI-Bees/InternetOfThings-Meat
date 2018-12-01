f1f41579a18852f9db5fc90a629d769d22126738
darkSideMatters <plamengetsov95@gmail.com>
2018-12-01 15:11:29 +0100

    joined the separate dataframes into 1 csv file

--- production_and_consumption/production_and_consumption.py
+++ production_and_consumption/production_and_consumption.py
@@ -6,22 +6,7 @@ production_dataframe = pd.read_csv(production_file_name, delimiter=",")
 
 consumption_file_name = "modifiedMeatConsumption.csv"
 consumption_dataframe = pd.read_csv(consumption_file_name, delimiter=",")
-consumption_country_list = consumption_dataframe.Country.unique().tolist()
-consumption_year_list = consumption_dataframe.Year.unique().tolist()
 
-# Matching on countries & year
-production_dataframe = production_dataframe[production_dataframe['Country'].isin(consumption_country_list)]
-production_dataframe = production_dataframe[production_dataframe['Year'].isin(consumption_year_list)]
+matching_data = pd.merge(left=production_dataframe, right=consumption_dataframe, on=['Country','Year'])
+matching_data.to_csv('joineddata.csv', sep =',')
 
-#production_country_list = production_dataframe.Country.unique().tolist()
-#production_year_list = production_dataframe.Year.unique().tolist()
-
-#consumption_dataframe = consumption_dataframe[consumption_dataframe['Country'].isin(production_country_list)]
-#consumption_dataframe = consumption_dataframe[consumption_dataframe['Year'].isin(production_year_list)]
-
-print(production_dataframe)
-
-def create_csv():
-    with open('production_and_consumption.csv', 'w', newline='') as f:
-        csv_writer = csv.writer(f)
-        csv_writer.writerow(['Country', 'Year', 'Production', 'Consumption'])
